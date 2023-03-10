4fc89e722fcc6a455fff1eda7f0e561d31c7b92a
Denzy7 <dennismyner7@gmail.com>
2023-03-10 11:57:15 +0300

    winsock2 + c89/90 sock

--- udpsh_sock.c
+++ udpsh_sock.c
@@ -1,43 +1,49 @@
 #include "udpsh_sock.h"
 
+#ifndef _WIN32
 #include <sys/socket.h> /* socket */
 #include <arpa/inet.h> /* inet_aton */
 #include <netdb.h> /* getaddrinfo */
 #include <unistd.h> /* close */
+#endif
+
+#ifdef _WIN32
+#define closesock(sock) closesocket(sock)
+#else
+#define closesock(sock) close(sock)
+#endif
 
 #include <stdio.h>
 #include <string.h>
 int udpsh_sock_make(const char* ipv4dest, struct udpsh_sock* udpsh_sock)
 {
+    struct addrinfo hints, *res;
+    int status;
+    struct sockaddr_in* ip4;
+    const char* ipstr = "0.0.0.0";
+
     if((udpsh_sock->sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
     {
         perror("cannot create socket");
         return 1;
     }
-    udpsh_sock->addr.sin_family = AF_INET;
-    if(ipv4dest == NULL)
-    {
-        udpsh_sock->addr.sin_addr.s_addr = INADDR_ANY;
-    }else
-    {
-        struct addrinfo hints, *res;
-        int status;
-        memset(&hints, 0, sizeof(hints));
+    ipstr = ipv4dest == NULL ? ipstr : ipv4dest;
 
-        hints.ai_family = AF_INET;
-        hints.ai_socktype = SOCK_STREAM;
-
-        if ((status = getaddrinfo(ipv4dest, NULL, &hints, &res)) != 0) {
-            fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
-            close(udpsh_sock->sock);
-            return 1;
-        }
-        struct sockaddr_in* ip4 = (struct sockaddr_in*)res->ai_addr;
-        memcpy(&udpsh_sock->addr.sin_addr,
-               &ip4->sin_addr,
-               sizeof(struct in_addr));
-        freeaddrinfo(res);
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    if ((status = getaddrinfo(ipstr, NULL, &hints, &res)) != 0) {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+        closesock(udpsh_sock->sock);
+        return 1;
     }
+    ip4 = (struct sockaddr_in*)res->ai_addr;
+    memcpy(&udpsh_sock->addr.sin_addr,
+           &ip4->sin_addr,
+           sizeof(struct in_addr));
+    freeaddrinfo(res);
+
+    udpsh_sock->addr.sin_family = AF_INET;
     udpsh_sock->addr.sin_port = htons(UDPSH_SOCK_PORT);
 
     /*printf("created socket at %s:%hu successfully\n",
@@ -49,7 +55,7 @@ int udpsh_sock_make(const char* ipv4dest, struct udpsh_sock* udpsh_sock)
 
 int udpsh_sock_close(const struct udpsh_sock* udpsh_sock)
 {
-    if(close(udpsh_sock->sock) < 0)
+    if(closesock(udpsh_sock->sock) < 0)
     {
         perror("udpsh_sock_close failed");
         return 1;
@@ -80,7 +86,7 @@ int udpsh_sock_recv(struct udpsh_sock* to, struct sockaddr_in* srcinfo, socklen_
 
 int udpsh_sock_send(const struct udpsh_sock* to)
 {
-    if(sendto(to->sock, to->buffer, UDPSH_SOCK_BUFSZ, MSG_CONFIRM,
+    if(sendto(to->sock, to->buffer, UDPSH_SOCK_BUFSZ, MSG_OOB,
            (const struct sockaddr*)&to->addr, sizeof(struct sockaddr_in)) == -1)
     {
         perror("udpsh_sock_send failed");
--- udpsh_sock.h
+++ udpsh_sock.h
@@ -1,13 +1,25 @@
 #ifndef UDPSH_SOCK_H
 #define UDPSH_SOCK_H
 
-#include <netinet/in.h> /* sockaddr_in, htons */
+/* sockaddr_in, socklen_t */
+#ifdef _WIN32
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#else
+#include <netinet/in.h>
+#endif
+
 #include <stddef.h> /* NULL, size */
 #define UDPSH_SOCK_PORT 0x3333
 #define UDPSH_SOCK_BUFSZ 1024
+
 struct udpsh_sock
 {
+#ifdef _WIN32
+    SOCKET sock;
+#else
     int sock;
+#endif
     struct sockaddr_in addr;
     socklen_t addrlen;
     char buffer[UDPSH_SOCK_BUFSZ];
@@ -19,4 +31,4 @@ int udpsh_sock_bind(const struct udpsh_sock* udpsh_sock);
 int udpsh_sock_recv(struct udpsh_sock* to, struct sockaddr_in* srcinfo, socklen_t* srcaddrlen);
 int udpsh_sock_send(const struct udpsh_sock* to);
 
-#endif // UDPSH_SOCK_H
+#endif /* UDPSH_SOCK_H */
